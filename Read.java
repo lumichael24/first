2f839c369737c21a42cac5ec5e0602b6c959840a
Michael <gongminglu@gmail.com>
2017-07-12 16:37:09 -0500

    Update Read.java

--- Read.java
+++ Read.java
@@ -11,12 +11,7 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
- 
-/**
- * A dirty simple program that reads an Excel file.
- * @author www.codejava.net
- *
- */
+
 public class Read {
      
     public static void main(String[] args) throws IOException {
